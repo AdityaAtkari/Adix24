45c4d8c59a2c1bfb644b16b16c921de7b0a1bc1a
Aditya Atkari <112647221+AdityaAtkari@users.noreply.github.com>
2023-12-17 08:41:56 +0000

    data type summary

---
+++ basics/datatype_summary.js
@@ -0,0 +1,21 @@
+//there is 7 type of primitive data types
+// the are 
+// String, Number, Boolean, Null, Undefined, BigInt, Symbol(is used to make things unique)
+// const anotherNumber = Symbol('123')
+const BigInt = 184451985154854n //bigInt declaration
+console.log(BigInt)
+
+// Reference (Non primitive)
+// that are Array, Objects, Functions 
+// inka return type(typeof) object aata hai function ka oject type aata hai
+
+const hereos = ["Shkatiman", "Hanuman", "Ghatotkach"];
+
+    let myObj = {
+        name: "Aditya",
+        age: 21,
+    }
+const myFunction = function(){
+console.log("hello world")
+}
+console.log(typeof BigInt)
